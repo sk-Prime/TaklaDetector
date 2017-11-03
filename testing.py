d4499f2180fe641b945405c4380b08f63e4337da
Sk Salim <sksalim000@yahoo.com>
2017-11-03 18:21:43 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
-from taklaGen import MuradTakla
+from BanglishGen import Banglish
 import pickle,os
 dict_path=os.getcwd()+"\\dictionary\\bdict.dat"
 
-converter=MuradTakla()
+converter=Banglish()
 
 with open(dict_path,"rb") as dfile:
     bdict=pickle.load(dfile)
@@ -10,6 +10,6 @@ with open(dict_path,"rb") as dfile:
 for i in range(2,100,3):
     words=bdict[i].split()
     for word in words:
-        takla=converter.BanglaToTakla(word)
+        takla=converter.BanglaToBanglish(word)
         print(word,":",takla)
     
