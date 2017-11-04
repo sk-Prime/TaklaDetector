04474839a800172f0af64311383efbcd4276c1c1
Sk Salim <sksalim000@yahoo.com>
2017-11-04 08:15:00 +0800

    Update BanglishGen.py

--- BanglishGen.py
+++ BanglishGen.py
@@ -4,7 +4,7 @@ class Banglish(object):
         self.chardict={'ষ্ঞ': 'shn', 'দ্ধ': 'ddh', 'ন্ধ': 'ndh', 'ক্ষ': 'kkh', 'ঞ্জ': 'ngj','ঙ্গ': 'ngg','ঋ': 'ri', 'জ্ঞ': 'gg', 'ঢ়': 'r', 'ক্ত': 'kt', 'ব্দ': 'bd', 'ট্ট': 'tt', 'স্ম': 'sm', 'গ্ন': 'gn', 'ন্দ': 'nd', 'ং': 'ng', 'ৎ': 'th', 'ভ': 'v', 'ষ': 's', 'শ': 'sh', 'ফ': 'f', 'ঢ': 'dh', 'ঠ': 'th', 'ধ': 'dh', 'থ': 'th', 'ঙ': 'ng', 'ঞ': 'ng', 'ঘ': 'gh', 'ন্ত': 'nt', 'ঝ': 'jh', 'খ': 'kh', 'ছ': 'ch', 'ঔ': 'ou', 'ঐ': 'oi', 'অ': 'o', 'আ': 'a', 'ই': 'i', 'ঈ': 'i', 'উ': 'u', 'ঊ': 'u', 'এ': 'e', 'ও': 'o', 'ক': 'k', 'গ': 'g', 'চ': 'ch', 'জ': 'j', 'ট': 't', 'ড': 'd', 'ণ': 'n', 'ত': 't', 'দ': 'd', 'ন': 'n', 'প': 'p', 'ব': 'b', 'ম': 'm', 'য': 'z', 'র': 'r', 'ল': 'l', 'স': 's', 'হ': 'h', 'ড়': 'r', 'য়': 'y'}
         self.vioel={'ি':'i','া': 'a', 'ী': 'i', 'ু': 'u', 'ূ': 'u', 'ৃ': 'rri', 'ে': 'e', 'ৈ': 'oi', 'ো': 'o', 'ৌ': 'ou'}
         self.vioel2="aeiou"
-        self.homoPhone={"sh":"s"}
+        self.homoPhone={"sh":"s","z":"j","rri":"ri","z":"y","okse":"khe","kso":"kho","bzo":"ba","zogz":"zogg"}
         
     def BanglaToBanglish(self,word):
         word=self.__correction(word)
@@ -30,9 +30,9 @@ class Banglish(object):
             taklaWord=taklaWord[1:]
         version=self.__homoPhone_version(taklaWord)
         if version!=taklaWord:
-            return [taklaWord,version]
+            return version
         return [taklaWord]
-    
+
     def __correction(self,word):
         need_to_correct=["্ব", "্ম"]
         corrected_word=""
@@ -45,15 +45,18 @@ class Banglish(object):
         return corrected_word
     
     def __homoPhone_version(self,word):
+        version=[]
         for key in self.homoPhone:
-            word=word.replace(key,self.homoPhone[key])
-        return word
+            temp=word.replace(key,self.homoPhone[key])
+            if temp not in version:
+                version.append(temp)
+        return version
 
 
 if __name__=="__main__":
-    test="আমাদের দেশ স্বপ্নপুরি বিশ্ব"
-    dictionary=taklaDictionary()
+    test="কবিতা, এক যে ছিল মজার দেশ সব রকমের ভাল রাত্তিরেতে বেজায় রোদ দিনে চাদের আলো আকাশ সেথা সবুজবরণ গাছের পাতা নীল ডাঙ্গায় চরে রুই কাতলা জলের মাঝে চিল"
+    dictionary=Banglish()
     for word in test.split():
-        takla=dictionary.BanglaToTakla(word)
-        print(takla)
+        banglish_word=dictionary.BanglaToBanglish(word)
+        print(banglish_word)
         
